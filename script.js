5c6fd8441a0219b4dd4d3ec5ff7b3d1ba7799ee0
kazukikikikii <e1922039@oit.ac.jp>
2025-06-03 11:20:38 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const character = document.getElementById('character');
+const speech = document.getElementById('speech');
+const messages = ["ようこそ！", "一緒に小説を作ろう！", "たのしんでね！"];
+let msgIndex = 0;
+
+character.addEventListener('click', () => {
+  speech.innerText = messages[msgIndex];
+  speech.style.display = "block";
+  msgIndex = (msgIndex + 1) % messages.length;
+  setTimeout(() => {
+    speech.style.display = "none";
+  }, 2000);
+});
