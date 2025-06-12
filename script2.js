35461b882723ce9150d0e82bc14c351509c180f1
kazukikikikii <e1922039@oit.ac.jp>
2025-06-12 12:42:04 +0900

    Update script2.js

--- script2.js
+++ script2.js
@@ -4,3 +4,16 @@ document.getElementById('ideaBtn').onclick = function() {
 document.getElementById('backBtn').onclick = function() {
   location.href = 'index2.html';
 };
+const character = document.getElementById('character');
+const speech = document.getElementById('speech');
+const messages = ["ようこそ！", "一緒に小説を作ろう！", "たのしんでね！"];
+let msgIndex = 0;
+
+character.addEventListener('click', () => {
+  speech.innerText = messages[msgIndex];
+  speech.style.display = "block";
+  msgIndex = (msgIndex + 1) % messages.length;
+  setTimeout(() => {
+    speech.style.display = "none";
+  }, 2000);
+});
