faa7846c46f88b8b55e96313929cc25e2c6ff9b7
kazukikikikii <e1922039@oit.ac.jp>
2025-06-12 12:31:49 +0900

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,3 +1,6 @@
 document.getElementById('ideaBtn').onclick = function() {
   location.href = 'index3.html';
 };
+document.getElementById('back-icon').onclick = function() {
+  location.href = 'index2.html';
+};
